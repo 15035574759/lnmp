1c1f82aa96ca5ddbcc4d680928b8b26773c0cf30
qinlh <qinlinhui@atai.net.cn>
2020-12-25 17:54:43 +0800

    修改启动脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,26 @@ set -e
 
 echo `service nginx status`
 echo '1.启动nginx....'
-service nginx start
+service nginx restart
 sleep 3
 echo `service nginx status`
 
 echo `service php-fpm status`
 echo '2.启动php-fpm....'
-service php-fpm start
+service php-fpm restart
 sleep 3
 echo `service php-fpm status`
 
-DATADIR='/usr/local/mysql/data'
+DATADIR='/data/mysql/data'
 chown -R mysql:mysql $DATADIR
 echo `service mysqld status`
 echo '3.启动mysqld....'
-service mysqld start
+service mysqld restart
 sleep 3
 echo `service mysqld status`
 
 echo `service redis status`
 echo '4.启动redis....'
-service redis start
+service redis restart
 sleep 3
 echo `service redis status`
